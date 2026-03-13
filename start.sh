3bec3566467c09a4af4005a670f2c628f8ac4999
spooky-may <decimasudo@gmail.com>
2026-03-14 00:27:58 +0700

    ci: tighten Docker publish workflow, update .gitignore, and clean startup script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-docker stop 9router
-docker rm 9router
-docker build -t 9router .
-docker run -d --name 9router -p 20128:20128 --env-file .env -v 9router-data:/app/data 9router
+docker stop lina-router
+docker rm lina-router
+docker build -t lina-router .
+docker run -d --name lina-router -p 20128:20128 --env-file .env -v lina-router-data:/app/data lina-router
